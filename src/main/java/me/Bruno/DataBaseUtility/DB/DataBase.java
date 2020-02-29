cd13e49f4f167472047414b7ff62ed92c4eaf70f
Bruno Moraes <bruno.moraes2@primecontrol.com.br>
2020-02-29 17:26:23 +0000

    Update DataBase.java

--- src/main/java/me/Bruno/DataBaseUtility/DB/DataBase.java
+++ src/main/java/me/Bruno/DataBaseUtility/DB/DataBase.java
@@ -1,8 +1,10 @@
 package me.Bruno.DataBaseUtility.DB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import me.Bruno.DataBaseUtility.DB.BaseTypes.Base;
@@ -15,15 +17,26 @@ public class DataBase {
     private DataBaseType type;
     private HikariDataSource ds;
     private Base base;
-    private String ip, port, database, login, password, path;
+    private String ip;
+    private String port;
+    private String database;
+    private String login;
+    private String password;
+    private String path;
+
+    public DataBase() {
+        build();
+    }
 
     public DataBase(String path) {
         this.type = DataBaseType.SQLITE;
         this.path = path;
+        build();
     }
 
     public DataBase(DataBaseType type) {
         this.type = type;
+        build();
     }
 
     public DataBase(DataBaseType type, String ip, String port, String database, String login, String password) {
@@ -33,78 +46,72 @@ public class DataBase {
         this.database = database;
         this.login = login;
         this.password = password;
+        build();
+    }
+
+    private void build() {
+        if (this.type != null) {
+            switch (this.type) {
+            case ORACLE:
+                this.base = new Oracle(this.ip, this.port, this.database, this.login, this.password);
+                break;
+            case MYSQL:
+                this.base = new MySQL(this.ip, this.port, this.database, this.login, this.password);
+                break;
+            case SQLITE:
+                this.base = new SQLite(this.path);
+                break;
+            }
+            this.ds = new HikariDataSource(base.getSource());
+        }
     }
 
     public DataBaseType getType() {
         return this.type;
     }
 
-    public Connection getConnection() {
-        if (ds == null || ds.isClosed()) {
-            buildSource();
-        }
-        try {
-            return ds.getConnection();
-        } catch (SQLException e) {
-            throw new IllegalStateException("Error during execution.", e);
-        }
-    }
-
-    private void buildSource() {
-        if (this.type != null) {
-            switch (this.type) {
-                case ORACLE:
-                    this.base = new Oracle(this.ip, this.port, this.database, this.login, this.password);
-                    break;
-                case MYSQL:
-                    this.base = new MySQL(this.ip, this.port, this.database, this.login, this.password);
-                    break;
-                case SQLITE:
-                    this.base = new SQLite(this.path);
-                    break;
-            }
-            this.ds = new HikariDataSource(base.getSource());
-        }
-    }
-
-    public HikariDataSource getDataSource() {
-        return this.ds;
-    }
-
     public <T> T execute(ConnectionCallback<T> callback) {
-    	if (ds == null || ds.isClosed()) {
-            buildSource();
-        }
-        Connection con = null;
-        try {
-            con = ds.getConnection();
+        try (Connection con = this.ds.getConnection()) {
             return callback.doInConnection(con);
-        } catch (SQLException e) {
-        	e.printStackTrace();
-            try {
-                if (con != null && !con.isClosed()) {
-                    con.rollback();
-                    con.close();
-                    if (ds != null && !ds.isClosed()) {
-                        this.ds.close();
-                        this.ds = null;
-                    }
-                }
-            } catch (Exception ex) {}
+        } catch (Exception e) {
             throw new IllegalStateException("Error during execution.", e);
-        } finally {
-            try {
-                if (con != null && !con.isClosed()) {
-                    con.close();
-                    if (ds != null && !ds.isClosed()) {
-                        this.ds.close();
-                        this.ds = null;
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    public <T> T execute(ConnectionCallback<T> callback, boolean ignoreError) {
+        try (Connection con = this.ds.getConnection()) {
+            return callback.doInConnection(con);
+        } catch (Exception e) {
+            if (!ignoreError) {
+                throw new IllegalStateException("Error during execution.", e);
+            }
+            return null;
+        }
+    }
+
+    public boolean hasTable(String tableName) {
+        String result = "" + execute(con -> {
+            PreparedStatement stm = null;
+            switch (type) {
+                case SQLITE:
+                case MYSQL:
+                    stm = con.prepareStatement("select * from " + tableName + " limit 1");
+                    break;
+                case ORACLE:
+                    stm = con.prepareStatement("select * from " + tableName + " where rownum = 1");
+                    break;
+            }
+            if (stm != null) {
+                try {
+                    stm.executeUpdate();
+                    return true;
+                } catch ( Exception e) {
+                    return false;
+                }
+            }
+            return false;
+        }, true);
+        return result.equalsIgnoreCase("null") ? false : true;
     }
 
     public static interface ConnectionCallback<T> {
