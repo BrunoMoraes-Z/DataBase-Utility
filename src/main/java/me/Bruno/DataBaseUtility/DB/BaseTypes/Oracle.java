cad451d6673d2781897b553fc1fd18e166443d0d
Bruno Moraes <bruno.moraes2@primecontrol.com.br>
2020-02-29 17:27:05 +0000

    Update Oracle.java

--- src/main/java/me/Bruno/DataBaseUtility/DB/BaseTypes/Oracle.java
+++ src/main/java/me/Bruno/DataBaseUtility/DB/BaseTypes/Oracle.java
@@ -9,12 +9,15 @@ public class Oracle extends Base {
     public Oracle(String ip, String port, String database, String username, String password) {
         source = new HikariConfig();
         source.setMaximumPoolSize(100);
-        source.setDataSourceClassName("oracle.jdbc.pool.OracleDataSource");
         source.addDataSourceProperty("serverName", ip);
         source.addDataSourceProperty("port", port != null ? port : "1521");
         source.addDataSourceProperty("databaseName", database != null ? database : "XE");
         source.addDataSourceProperty("user", username);
         source.addDataSourceProperty("password", password);
+        source.setConnectionTestQuery("SELECT 1 FROM DUAL");
+        source.setJdbcUrl("jdbc:oracle:thin:@//" + ip + ":" + (port != null ? port : "1521") + "/" + (database != null ? database : "XE"));
+        source.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        source.setAutoCommit(true);
     }
 
     public HikariConfig getSource() {
